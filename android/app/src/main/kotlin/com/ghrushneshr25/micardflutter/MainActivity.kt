ac6a30fa1a1b7a169cd175fb9e4e8851000a4ab2
Ghrushnesh Rathod <ghrushneshr24@gmail.com>
2021-01-24 23:36:32 +0530

    Lecture 43

---
+++ android/app/src/main/kotlin/com/ghrushneshr25/micardflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ghrushneshr25.micardflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          // child: Column(
+          //   // verticalDirection: VerticalDirection.up,
+          //   // mainAxisAlignment: MainAxisAlignment.end,
+          //   //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //   // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   // crossAxisAlignment: CrossAxisAlignment.end,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: <Widget>[
+          //       Container(
+          //         height: 100,
+          //         width: 100,
+          //         color: Colors.white,
+          //         child: Text("Container 1")
+          //       ),
+          //       Container(
+          //         height: 100,
+          //         width: 200,
+          //         color: Colors.blue,
+          //         child: Text("Container 2")
+          //       ),
+          //       Container(
+          //         height: 100,
+          //         width: 300,
+          //         color: Colors.red,
+          //         child: Text("Container 3")
+          //       )
+          //     ],
+          //   ),
+
+          // LECTURE: 43 CHALLENGE
+          // child: Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   children: <Widget>[
+          //     Container(
+          //       width: 100,
+          //       height: double.infinity,
+          //       color: Colors.redAccent,
+          //     ),
+          //     Column(
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       children: [
+          //         SizedBox(),
+          //         Container(
+          //           height: 100,
+          //           width: 100,
+          //           color: Colors.yellow,
+          //         ),
+          //         Container(
+          //           height: 100,
+          //           width: 100,
+          //           color: Colors.green,
+          //         ),
+          //         SizedBox(),
+          //       ],
+          //     ),
+          //     Container(
+          //       width: 100,
+          //       height: double.infinity,
+          //       color: Colors.redAccent,
+          //     ),
+          //   ],
+          // ),
+          ),
+        ),
+      );
+  }
+}
+
+
